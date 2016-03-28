333460716b4b3f78ef6b4707d9b25d0964066c64
Chapelskyi Slavik <slavaternopil@ukr.net>
2016-03-28 17:59:05 +0300

    AddWinFormsFiles

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(Object sender, EventArgs e)
+        {
+            ShowMessage(
+                Handle,
+                "Hello from MessageBox function",
+                "Hello",
+                MBType.MB_ABORTRETRYIGNORE);
+        }
+
+        [Flags]
+        private enum MBType : uint
+        {
+            MB_ABORTRETRYIGNORE = 0x00000002,
+            MB_YESNO = 0x00000004
+        }
+
+        [DllImport("user32.dll", EntryPoint = "MessageBox")]
+        private static extern Int32 ShowMessage(
+            IntPtr hWnd,
+            string lpText,
+            string lpCaption,
+            MBType uType);
+
+    }
+}
